c118381b43e0f164615e6d2304e325d23db6601f
Salvador Gibert <salvigibert@gmail.com>
2019-08-06 12:08:04 -0300

    Commit2/Salvador

--- src/aerolineas/Aerolineas.java
+++ src/aerolineas/Aerolineas.java
@@ -16,7 +16,9 @@ public class Aerolineas {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola jejejejejjejejee");
+        
+        NewJFrame login1 = new NewJFrame();
+        login1.setVisible(true);
     }
     
 }
